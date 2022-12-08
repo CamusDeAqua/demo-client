a498a63115fc9ad08b2352bc249dd4a266156e7d
CamusDeAqua <bruniitoflow@gmail.com>
2022-12-08 18:51:06 -0300

    lalala

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { store } from './store';
 import axios from 'axios';
 //dotenv.config();
 
-axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
+axios.defaults.baseURL = "https://ps-ch-deploy.herokuapp.com" || "http://localhost:3001";
 
 ReactDOM.render(
   <Provider store={store}>
